c4041d36c2ed5faee30c9539e40e664c51dae26f
Yosua Risandy <jojo@kata.ai>
2021-11-29 01:46:48 +0700

    fix: api repelaced

--- pages/api/sites.js
+++ pages/api/sites.js
@@ -1,50 +1,28 @@
-import useSWR from 'swr';
+import { auth } from '@/lib/firebase-admin';
+import { getUserSites } from '@/lib/db-admin';
+import { logger, formatObjectKeys } from '@/utils/logger';
 
-import { useAuth } from '@/lib/auth';
-import fetcher from '@/utils/fetcher';
-import Page from '@/components/Page';
-import DashboardShell from '@/components/DashboardShell';
-import SiteTable from '@/components/SiteTable';
-import SiteEmptyState from '@/components/SiteEmptyState';
-import SiteTableHeader from '@/components/SiteTableHeader';
-import SiteTableSkeleton from '@/components/SiteTableSkeleton';
-import UpgradeEmptyState from '@/components/UpgradeEmptyState';
+export default async (req, res) => {
+    try {
+        const { uid } = await auth.verifyIdToken(req.headers.token);
+        const { sites } = await getUserSites(uid);
 
-const Dashboard = () => {
-  const { user } = useAuth();
-  const { data } = useSWR(user ? ['/api/sites', user.token] : null, fetcher);
-  const isPaidAccount = user?.stripeRole !== 'free';
+        res.status(200).json({ sites });
+    } catch (error) {
+        logger.error(
+            {
+                request: {
+                    headers: formatObjectKeys(req.headers),
+                    url: req.url,
+                    method: req.method
+                },
+                response: {
+                    statusCode: res.statusCode
+                }
+            },
+            error.message
+        );
 
-  if (!data) {
-    return (
-      <DashboardShell>
-        <SiteTableHeader />
-        <SiteTableSkeleton />
-      </DashboardShell>
-    );
-  }
-
-  if (data.sites.length) {
-    return (
-      <DashboardShell>
-        <SiteTableHeader isPaidAccount={isPaidAccount} />
-        <SiteTable sites={data.sites} />
-      </DashboardShell>
-    );
-  }
-
-  return (
-    <DashboardShell>
-      <SiteTableHeader isPaidAccount={isPaidAccount} />
-      {isPaidAccount ? <SiteEmptyState /> : <UpgradeEmptyState />}
-    </DashboardShell>
-  );
+        res.status(500).json({ error });
+    }
 };
-
-const DashboardPage = () => (
-  <Page name="Dashboard" path="/sites">
-    <Dashboard />
-  </Page>
-);
-
-export default DashboardPage;
